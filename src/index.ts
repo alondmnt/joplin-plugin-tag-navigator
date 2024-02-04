9a4310657433c76af2f6cc0dfc7f81f0925af2d5
alondmnt <alondmnt@users.noreply.github.com>
2024-02-04 23:29:36 +1100

    new: command convertAllNotesToJoplinTags

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import { convertNoteToJoplinTags } from './converter';
 joplin.plugins.register({
   onStart: async function() {
 
-    // Register the command
     await joplin.commands.register({
       name: 'itags.convertNoteToJoplinTags',
       label: "Convert note's inline tags to Joplin tags",
@@ -16,5 +15,30 @@ joplin.plugins.register({
         await convertNoteToJoplinTags(note);
       },
     });
+
+    await joplin.commands.register({
+      name: 'itags.convertAllNotesToJoplinTags',
+      label: "Convert all notes' inline tags to Joplin tags",
+      iconName: 'fas fa-tags',
+      execute: async () => {
+        // Get all notes
+        let hasMore = true;
+        let page = 0;
+        while (hasMore) {
+          const notes = await joplin.data.get(['notes'], {
+            fields: ['id', 'body'],
+            limit: 50,
+            page: page++,
+          });
+          hasMore = notes.has_more;
+
+          // Process the notes asynchronously
+          await Promise.all(notes.items.map(async (note) => {
+            await convertNoteToJoplinTags(note);
+          }));
+        }
+      },
+    });
+
   },
 });
