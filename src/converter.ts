66c82b166483c14714a5c17dc18fc6582ae928fa
alondmnt <alondmnt@users.noreply.github.com>
2024-02-05 00:13:31 +1100

    refactor: converter

--- src/converter.ts
+++ src/converter.ts
@@ -1,6 +1,25 @@
 import joplin from 'api';
 import { parseUniqueTags } from './parser';
 
+export async function convertAllNotesToJoplinTags() {
+  // Get all notes
+  let hasMore = true;
+  let page = 0;
+  while (hasMore) {
+    const notes = await joplin.data.get(['notes'], {
+      fields: ['id', 'body'],
+      limit: 50,
+      page: page++,
+    });
+    hasMore = notes.has_more;
+
+    // Process the notes asynchronously
+    await Promise.all(notes.items.map(async (note) => {
+      await convertNoteToJoplinTags(note);
+    }));
+  }
+}
+
 export async function convertNoteToJoplinTags(note: any) {
 
   // Prase all inline tags from the note
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import joplin from 'api';
-import { convertNoteToJoplinTags } from './converter';
+import { convertAllNotesToJoplinTags, convertNoteToJoplinTags } from './converter';
 
 joplin.plugins.register({
   onStart: async function() {
@@ -21,22 +21,7 @@ joplin.plugins.register({
       label: "Convert all notes' inline tags to Joplin tags",
       iconName: 'fas fa-tags',
       execute: async () => {
-        // Get all notes
-        let hasMore = true;
-        let page = 0;
-        while (hasMore) {
-          const notes = await joplin.data.get(['notes'], {
-            fields: ['id', 'body'],
-            limit: 50,
-            page: page++,
-          });
-          hasMore = notes.has_more;
-
-          // Process the notes asynchronously
-          await Promise.all(notes.items.map(async (note) => {
-            await convertNoteToJoplinTags(note);
-          }));
-        }
+        await convertAllNotesToJoplinTags();
       },
     });
 
