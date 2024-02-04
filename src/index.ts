d78ff755d2b92b56637a760492e30767b154b334
alondmnt <alondmnt@users.noreply.github.com>
2024-02-04 23:21:19 +1100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import joplin from 'api';
+import { convertNoteToJoplinTags } from './converter';
 
 joplin.plugins.register({
   onStart: async function() {
@@ -12,47 +13,7 @@ joplin.plugins.register({
         // Get the selected note
         const note = await joplin.workspace.selectedNote();
 
-        // Prase all inline tags from the note
-        const tags = note.body.match(/#(\w+)/g).map(tag => {
-          return tag.replace('#', '');
-        });
-
-        // Get note tags
-        const noteTags = await joplin.data.get(['notes', note.id, 'tags'], { fields: ['id', 'title'] });
-        const noteTagNames = noteTags.items.map(tag => tag.title);
-        const tagsToAdd = tags.filter(tag => !noteTagNames.includes(tag));
-
-        console.log('noteTags', noteTags);
-        console.log('tagsToAdd', tagsToAdd);
-
-        if (tagsToAdd.length === 0) {
-          return;
-        }
-
-        // Get the existing tags
-        const allTags = await joplin.data.get(['tags'], { fields: ['id', 'title'] });
-        const allTagNames = allTags.items.map(tag => tag.title);
-
-        // Create the tags that don't exist
-        const curTags = allTags.items.filter(tag => tagsToAdd.includes(tag.title));
-        const newTags = tagsToAdd.filter(tag => !allTagNames.includes(tag));
-        console.log('curTags', curTags);
-        console.log('newTags', newTags);
-
-        for (const tag of newTags) {
-          const newTag = await joplin.data.post(['tags'], null, { title: tag });
-          // Update the note tags
-          await joplin.data.post(['tags', newTag.id, 'notes'], null, {
-            id: note.id
-          });
-        }
-
-        for (const tag of curTags) {
-          // Update the note tags
-          await joplin.data.post(['tags', tag.id, 'notes'], null, {
-            id: note.id
-          });
-        }
+        await convertNoteToJoplinTags(note);
       },
     });
   },
