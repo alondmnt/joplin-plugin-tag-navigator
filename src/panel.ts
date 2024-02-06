9bb133c070cf28477955ea67f0524f7abaab7619
alondmnt <alondmnt@users.noreply.github.com>
2024-02-06 17:51:26 +1100

    improve: panel displays next tag location

--- src/panel.ts
+++ src/panel.ts
@@ -2,9 +2,13 @@ import joplin from 'api';
 
 export async function updatePanel(panel: string, tagsLines: { tag: string, lines: number[], count: number, index: number }[]) {
   const html = tagsLines.map((tag) => {
+    let indexText = '';
+    if (tag.count > 1) {
+      indexText = `(${tag.index+1}/${tag.count})`;
+    }
     return `
       <a class="itags-panel-tag" href="#" data-tag="${tag.tag}" data-line="${tag.lines[tag.index]}">
-      #${tag.tag} (${tag.count})
+      #${tag.tag} ${indexText}
       </a>
     `;
   }).join('');
