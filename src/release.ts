62b94ade246e06a9682dfcf17b0ba74be34e9cf7
alondmnt <alondmnt@users.noreply.github.com>
2025-03-24 21:25:33 +0200

    version: v2.3.2

--- src/release.ts
+++ src/release.ts
@@ -1,11 +1,11 @@
 export const RELEASE_NOTES = {
-    version: "2.3.0",
-    notes: `Inline Tag Navigator v2.3.0
+    version: "v2.3.2",
+    notes: `Inline Tag Navigator v2.3.2
 
-- improved: tag parsing regex to support multilingual tag names. if you are using a custom regex, consider updating it based on the README
-- improved: support filtering results with regex in note views (when setting is enabled)
-- fixed: regression: convert all notes to joplin tags
-- fixed: removed extra slash from notebook path
-- fixed: escape regex characters in note view highlights
-- fixed: escape regex characters in table processing`,
+    - fixed: release notes freeze when Joplin starts minimised
+    - improved: command descriptions
+    - improved: command order in 'Tag Navigator' sub-menu
+    - added: Note sub-menu 'Convert all notes'
+    - added: Note sub-menu 'Convert current note'
+    - improved: error handling when editor is unavailable`,
   };
