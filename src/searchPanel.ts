88cd17c70cd82bb775896115ccf67b60ea564155
alondmnt <alondmnt@users.noreply.github.com>
2025-05-04 22:54:36 +1000

    improved: refresh note view after save query

--- src/searchPanel.ts
+++ src/searchPanel.ts
@@ -202,6 +202,7 @@ export async function processMessage(
     clearObjectReferences(currentNote);
 
     await saveQuery({query: JSON.parse(message.query), filter: message.filter, displayInNote: currentQuery.displayInNote});
+    await joplin.commands.execute('itags.refreshNoteView');
 
   } else if (message.name === 'openNote') {
     try {
