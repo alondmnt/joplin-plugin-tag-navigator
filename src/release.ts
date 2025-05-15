0dd99af580d5653e5cdfa9291d323040dbeed332
alondmnt <alondmnt@users.noreply.github.com>
2025-05-15 21:59:52 +1000

    version: v2.4.0

--- src/release.ts
+++ src/release.ts
@@ -1,11 +1,19 @@
 export const RELEASE_NOTES = {
-    version: "v2.3.2",
-    notes: `Inline Tag Navigator v2.3.2
+    version: "v2.4.0",
+    notes: `Inline Tag Navigator v2.4.0
 
-    - fixed: release notes freeze when Joplin starts minimised
-    - improved: command descriptions
-    - improved: command order in 'Tag Navigator' sub-menu
-    - added: Note sub-menu 'Convert all notes'
-    - added: Note sub-menu 'Convert current note'
-    - improved: error handling when editor is unavailable`,
-  };
+- added: 'Search: Result grouping' setting, which determines how results are split into sections: group by heading (default); group consecutive lines (legacy behaviour); split at each item / paragraph. grouping affects the behaviour of search filters
+- added: separate settings 'Note view: Highlight filter results' and 'Search: Highlight filter / tag results'
+- added: 'Search: Wait for note period (ms)' setting, that you may decrease to scroll faster to note lines
+- added: quick link (arrow) to the first result in each note
+- added: 'Note view: Display colors' setting, to use color tags for titles in note view
+- improved: a click on any line in the result list scrolls to that line in the note
+- improved: expanded/collapsed state preserved on mobile
+- improved: on mobile, dismiss the plugin panel when opening a note
+- improved: tag ranges logic
+- improved: render soft line breaks as hard line breaks
+- improved: update the note view when saving a query
+- improved: open external links, and links to note headings
+- improved: support wikilinks to zettel IDs or first word in title
+- fixed: tag parsing in the beginning of a line
+`};
