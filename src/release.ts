319fd479632a8ade4a261d4608c5f81cdc50d437
alondmnt <alondmnt@users.noreply.github.com>
2025-11-14 12:07:12 +1100

    version: v2.6.3

--- src/release.ts
+++ src/release.ts
@@ -1,19 +1,16 @@
 export const RELEASE_NOTES = {
-    version: "v2.6.1",
-    notes: `v2.6.1:
+    version: "v2.6.3",
+    notes: `v2.6.3:
+v2.6.3:
+- added: nested tag support for date tags
+- improved: memory management
+
+v2.6.*:
 - improved: MD preview tag rendering supports custom regexes
 - improved: search panel tag rendering with separate CSS classes for each tag prefix
 - fixed: search panel toggling
-
-v2.6.0:
 - added: render tags in the Markdown preview (see settings)
 - added: render front matter in the Markdown preview (see settings)
 - added: highlight front matter in the Markdown editor (see settings)
 - fixed: front matter parsing logic
-
-other features since v2.5.0:
-- added: search results filter with exclusion patterns, beginning with "!"
-- added: include/exclude notebooks from the tag DB (see settings)
-- added: middle matter support (see settings)
-- added: toggle navigation panel keyboard shortcut
-- improved: joplin tags to inline tags conversion`};
+`};
